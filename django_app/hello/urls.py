d053f2fa07175e2585f1af6468434904faab1994
degiple <degiple@gmail.com>
2020-05-20 21:28:21 +0900

    Update a form

--- django_app/hello/urls.py
+++ django_app/hello/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('form', views.form, name='form'),
+    # path('form', views.form, name='form'),
     path('query', views.query, name='query'),
     path('<int:id>/<nickname>/', views.query_smart, name='query_smart'),
     path('My_name_is_<name>.I_am_<int:age>_years_old.', views.me, name='me')
