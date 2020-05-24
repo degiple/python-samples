1aaba67339f80ab596576c963e3b393317a25a9f
degiple <degiple@gmail.com>
2020-05-24 16:00:27 +0900

    Completed '[Django Rest] Tutorial 1: Serialization

--- django_rest/django_rest/urls.py
+++ django_rest/django_rest/urls.py
@@ -1,4 +1,4 @@
-"""django_app URL Configuration
+"""django_rest URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -13,21 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-# from django.conf.urls import url
+from django.urls import include, path
 from rest_framework import routers
-from rest import views
-# import hello.views as hello
+from quickstart import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('hello/', include('hello.urls')),
-    path('sns/', include('sns.urls')),
-    path('todo/', include('todo.urls')),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include('snippets.urls')),
 ]
